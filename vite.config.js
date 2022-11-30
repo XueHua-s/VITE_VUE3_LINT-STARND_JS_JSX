633f52cc08b59b2311d88c00fae13b4ce682ce74
李嘉欣 <1006084794@qq.com>
2022-11-30 08:32:22 +0800

    增加require

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import requireTransform from 'vite-plugin-require-transform'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,6 +11,9 @@ export default defineConfig({
     vueJsx(),
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+    }),
+    requireTransform({
+      fileRegex: /.js$|.vue/
     })
   ],
   resolve: {
